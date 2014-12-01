4813cf0c339f26826505823cfc23b80199746ab8
dgiraldo <dgiraldo@smdigitalpartners.com>
2014-12-01 15:17:29 -0500

    este es un commit de prueba del nuevo branch

--- zip_search.php
+++ zip_search.php
@@ -17,6 +17,6 @@ $zip_array = [28239 => 'Gulf Coast Self Storage',
 if(array_key_exists($_REQUEST['zip'], $zip_array)){
 	echo $zip_array[$_REQUEST['zip']];
 }else{
-	echo "Is not a valid zip";
+	echo "Is not a valid zipiiiii";
 }
 ?>
